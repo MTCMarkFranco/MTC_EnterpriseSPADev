77bab8144e440081f0d718541264b43caa0f818f
Mark Franco <marfra@microsoft.com>
2018-03-02 16:38:38 -0500

    Added Build Number to Header

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 var htmlmin = require('gulp-htmlmin');
+var log = require('fancy-log');
+
+
 
 gulp.task('js:minify', function () {
   gulp.src(["./dist/main.*.js", "./dist/polyfills.*.js", "./dist/inline.*.js"])
@@ -24,4 +27,16 @@ gulp.task("html:minify", function () {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task("default", ["js:minify", "html:minify", "web:config", "apache:htaccess"]);
+gulp.task('build:versioninfo', function (){
+
+   require('dotenv').load();
+  var versionString = process.env.BuildVersion;
+
+  return gulp.src('dist/index.html')
+    .pipe(replace("<span id=\"BuildVersionNumber\"></span>", "<span id=\"BuildVersionNumber\">" + versionString + "</span>"))
+    .pipe(gulp.dest('./dist'));
+
+}  
+);
+
+gulp.task("default", ["js:minify", "html:minify", "web:config", "apache:htaccess","build:versioninfo"]);
